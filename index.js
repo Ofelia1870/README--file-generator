ff13c6ad00a269976afa0284ef3130ce805f9a1c
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-04-22 12:36:56 -0700

    linked inquirer prompt to questions variable array

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your title?",
+    name: "titleName",
+  },
+];
+
+inquirer.prompt(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
